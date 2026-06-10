d3e9401f63c577e9227e94839ed56cb55fc9c99e
Arpan Jana <125195972+ArpanJana2002@users.noreply.github.com>
2026-06-10 20:39:42 +0530

    Refactor WebDriver setup and teardown methods

--- src/main/java/com/blazedemo/base/BaseTest.java
+++ src/main/java/com/blazedemo/base/BaseTest.java
@@ -1,8 +1,6 @@
 package com.blazedemo.base;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,31 +8,24 @@ import com.blazedemo.utils.ConfigReader;
 import com.blazedemo.utils.DriverFactory;
 
 public class BaseTest {
-    protected WebDriver driver;
 
-    @BeforeMethod
-    public void setup() {
-    // Initialize Chrome options
-    ChromeOptions options = new ChromeOptions();
-    options.setBinary("/usr/bin/chromium-browser"); // Use the CI environment path
-    options.addArguments("--start-maximized");
-    options.addArguments("--disable-blink-features=AutomationControlled");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    
-    // Initialize WebDriver
-    WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver(options);
-    
-    // Navigate to base URL
-    driver.navigate().to(ConfigReader.get("baseURL"));
-}
+	protected WebDriver driver;
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	@BeforeMethod
+	public void setup() {
+
+		driver = DriverFactory.getDriver();
+
+		driver.get(ConfigReader.get("url"));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+
+		if (driver != null) {
+
+			driver.quit();
+		}
+	}
 
 }

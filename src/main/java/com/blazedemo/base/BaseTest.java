86bed79ef3820d9c600f5c0d245aeef120ba9858
Arpan Jana <125195972+ArpanJana2002@users.noreply.github.com>
2026-06-10 20:29:46 +0530

    Update BaseTest.java

--- src/main/java/com/blazedemo/base/BaseTest.java
+++ src/main/java/com/blazedemo/base/BaseTest.java
@@ -1,29 +1,34 @@
 package com.blazedemo.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.blazedemo.utils.ConfigReader;
 import com.blazedemo.utils.DriverFactory;
-protected WebDriver driver;
 
-@BeforeTest
-public void setup() {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    
-    driver = new ChromeDriver(options);
-    driver.manage().window().maximize();
-    driver.get(ConfigReader.get("baseUrl"));
-}
+public class BaseTest {
+    protected WebDriver driver;
 
-@AfterTest
-public void teardown() {
-    if (driver != null) {
-        driver.quit();
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.get("baseUrl"));
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }

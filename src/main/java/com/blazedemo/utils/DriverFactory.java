2e75f7d8887d7a67f53f1f8a6bdafdc82ac4dd2b
Arpan Jana <125195972+ArpanJana2002@users.noreply.github.com>
2026-06-10 21:00:48 +0530

    Update DriverFactory.java

--- src/main/java/com/blazedemo/utils/DriverFactory.java
+++ src/main/java/com/blazedemo/utils/DriverFactory.java
@@ -2,21 +2,33 @@ package com.blazedemo.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-	private DriverFactory() {
-	}
 
-	public static WebDriver getDriver() {
+    private DriverFactory() {
+    }
 
-		WebDriverManager.chromedriver().setup();
+    public static WebDriver getDriver() {
 
-		WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
 
-		driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
 
-		return driver;
-	}
+        if (System.getenv("GITHUB_ACTIONS") != null) {
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().window().maximize();
+
+        return driver;
+    }
 }
